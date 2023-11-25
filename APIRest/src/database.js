3ee440855de4fea32e64be8746d34559448d0fdd
GuimaraesSl <gabriel.alsamir@gmail.com>
2023-11-25 17:14:59 -0300

    finished CRUD Assunto

--- APIRest/src/database.js
+++ APIRest/src/database.js
@@ -14,6 +14,7 @@ async function connect(){
 
 connect();
 
+// ====================== CRUD JOGADOR ========================================
 async function selectJogadores(){
   const client = await connect();
   const res = await client.query('SELECT * FROM Jogador;');
@@ -37,8 +38,74 @@ async function insertJogador(jogador){
   await client.query(sql, values);
 }
 
+async function updateJogador(nickname, jogador){
+  const client = await connect();
+  const sql = `
+    UPDATE Jogador SET nickname=$1, login=$2, senha=$3, turma=$4, ano_escolar=$5, score=$6
+    WHERE nickname=$7;  
+  `
+  const values = [jogador.nickname, jogador.login, jogador.senha, jogador.turma, jogador.ano_escolar, jogador.score, nickname];
+  await client.query(sql, values);
+}
+
+async function deleteJogador(nickname){
+  const client = await connect();
+  const sql = `DELETE FROM Jogador WHERE nickname=$1;`
+  const values = [nickname];
+  await client.query(sql, values);
+}
+
+
+// ============================ CRUD ASSUNTO ==============================
+async function selectAssuntos(){
+  const client = await connect();
+  const res = await client.query('SELECT * FROM Assunto;');
+  return res.rows;
+}
+
+async function selectAssunto(id){
+  const client = await connect();
+  const res = await client.query('SELECT * FROM Assunto WHERE id_assunto=$1', [id]);
+  return res.rows;
+}
+
+async function insertAssunto(assunto){
+  const client = await connect();
+  const sql = `
+    INSERT INTO Assunto (nome, operador)
+    VALUES ($1, $2)
+    RETURNING *;
+  `
+  const values = [assunto.nome, assunto.operador];
+  await client.query(sql, values);
+}
+
+async function updateAssunto(id, assunto){
+  const client = await connect();
+  const sql = `
+    UPDATE Assunto SET nome=$1, operador=$2
+    WHERE id_assunto=$3;  
+  `
+  const values = [assunto.nome, assunto.operador, id];
+  await client.query(sql, values);
+}
+
+async function deleteAssunto(id){
+  const client = await connect();
+  const sql = `DELETE FROM Assunto WHERE id_assunto=$1;`
+  const values = [id];
+  await client.query(sql, values);
+}
+
 module.exports = {
   selectJogadores,
   selectJogador,
-  insertJogador
+  insertJogador,
+  updateJogador,
+  deleteJogador,
+  selectAssuntos,
+  selectAssunto,
+  insertAssunto,
+  updateAssunto,
+  deleteAssunto
 }
--- APIRest/src/index.js
+++ APIRest/src/index.js
@@ -7,32 +7,106 @@ const app = express();
 app.use(express.json());
 app.listen(PORT, () => console.log(`Server runnig on port ${PORT}`));
 
+// ============================= CRUD JOGADORES ================================
 // ROTA SELECT JOGADORES
-app.get('/jogadores', async (req, res) => {
+app.get('/jogador', async (req, res) => {
   try {
     const asw = await db.selectJogadores();
-    return res.status(200).send(asw)
+    return res.status(200).send(asw);
   } catch {err} {
-    return res.status(400).send(err)
+    return res.status(400).send(err);
   }
 })
 
 // ROTA JOGADOR BY ID
-app.get('/jogadores/:nickname', async (req, res) => {
+app.get('/jogador/:nickname', async (req, res) => {
   try {
     const asw =  await db.selectJogador(req.params.nickname);
     return res.status(200).send(asw);
   } catch(err) {
-    return res.status(400).send(err)
+    return res.status(400).send(err);
   }
 })
 
 // ROTA INSERT JOGADOR
-app.post('/session', async (req, res) => {
+app.post('/jogador', async (req, res) => {
   try {
-    await db.insertJogador(req.body)
-    return res.sendStatus(201);
+    await db.insertJogador(req.body);
+    return res.sendStatus(200);
   } catch(err) {
     return res.status(400).send(err);
   }
 })
+
+// ROTA UPDATE JOGADOR
+app.patch("/jogador/:nickname", async (req, res) => {
+  try {
+    await db.updateJogador(req.params.nickname, req.body);
+    return res.sendStatus(200);
+  } catch(err) {
+    return res.status(400).send(err);
+  }
+})
+
+// ROTA DELETE JOGADOR
+app.delete("/jogador/:nickname", async (req, res) => {
+  try {
+    await db.deleteJogador(req.params.nickname);
+    return res.sendStatus(204);
+  } catch(err) {
+    return res.status(400).send(err);
+  }
+})
+
+//==================== CRUD ASSUNTO ==========================
+// ROTA SELECIONA ASSUNTOS
+app.get('/assunto', async (req, res) => {
+  try {
+    const asw = await db.selectAssuntos();
+    return res.status(200).send(asw);
+  } catch {err} {
+    return res.status(400).send(err);
+  }
+})
+
+// ROTA ASSUNTO BY ID
+app.get('/assunto/:id', async (req, res) => {
+  try {
+    const asw =  await db.selectAssunto(req.params.id);
+    return res.status(200).send(asw);
+  } catch(err) {
+    return res.status(400).send(err);
+  }
+})
+
+// ROTA INSERT ASSUNTO
+app.post('/assunto', async (req, res) => {
+  try {
+    await db.insertAssunto(req.body);
+    return res.sendStatus(200);
+  } catch(err) {
+    return res.status(400).send(err);
+  }
+})
+
+// ROTA UPDATE ASSUNTO
+app.patch("/assunto/:id", async (req, res) => {
+  try {
+    await db.updateAssunto(req.params.id, req.body);
+    return res.sendStatus(200);
+  } catch(err) {
+    return res.status(400).send(err);
+  }
+})
+
+// ROTA DELETE JOGADOR
+app.delete("/assunto/:id", async (req, res) => {
+  try {
+    await db.deleteAssunto(req.params.id);
+    return res.sendStatus(204);
+  } catch(err) {
+    return res.status(400).send(err);
+  }
+})
+
+
